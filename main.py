60098ddd4c0ae249f31e56718b646c715f09d122
Thomas Moulard <thomas.moulard@gmail.com>
2008-10-19 16:02:19 +0200

    Bank, account and operation pages.

--- main.py
+++ main.py
@@ -15,25 +15,227 @@
 # limitations under the License.
 #
 
+import os
 
+from decimal import *
 
-
-import wsgiref.handlers
-
-
+from google.appengine.ext.webapp import template
+from google.appengine.api import users
 from google.appengine.ext import webapp
+from google.appengine.ext import db
+from google.appengine.ext.webapp.util import run_wsgi_app
 
+class DecimalProperty(db.Property):
+  data_type = Decimal
 
-class MainHandler(webapp.RequestHandler):
+  def get_value_for_datastore(self, model_instance):
+    value = super(DecimalProperty,
+                  self).get_value_for_datastore(model_instance)
+    return value
+
+  def make_value_from_datastore(self, value):
+    if value is None:
+      return None
+    return value
+
+  def validate(self, value):
+    v = Decimal(value)
+    if value is not None and not isinstance(v, Decimal):
+      raise BadValueError('Property %s must be convertible '
+                          'to a Decimal instance (%s)' %
+                          (self.name, value))
+    return super(DecimalProperty, self).validate(value)
+
+  def empty(self, value):
+    return not value
+
+class Bank(db.Model):
+  name = db.StringProperty(required=True)
+
+class Account(db.Model):
+  name = db.StringProperty(required=True)
+  bank = db.ReferenceProperty(Bank, required=True)
+  dateCreation = db.DateTimeProperty(auto_now_add=True)
+  dateUpdate = db.DateTimeProperty(auto_now=True)
+  owner = db.UserProperty(required=True)
+
+class Operation(db.Model):
+  description = db.StringProperty()
+  account = db.ReferenceProperty(required=True)
+  amount = DecimalProperty(required=True)
+
+class AccountPage(webapp.RequestHandler):
+  def add(self):
+    if users.get_current_user() == None:
+      return
+    account = Account(
+      name = self.request.get('account'),
+      bank = db.get(self.request.get('bank')),
+      owner = users.get_current_user(),
+      )
+    account.put()
+
+  def delete(self):
+    if users.get_current_user() == None:
+      return
+    if self.request.get('key') == None:
+      return
+    account = db.get(self.request.get('key'))
+    if account.owner == users.get_current_user():
+      account.delete()
+
+  def handleActions(self):
+    if self.request.get('action') == 'delete':
+      self.delete()
+      self.redirect('/')
+    if self.request.get('action') == 'add':
+      self.add()
+      self.redirect('/')
 
   def get(self):
-    self.response.out.write('Hello world!')
+    self.handleActions()
+
+    if users.get_current_user() == None:
+      accounts = None
+    else:
+      accounts_query = Account.all()
+      accounts_query.filter('owner =', users.get_current_user()).order('name')
+      accounts = accounts_query.fetch(10)
+
+    banks_query = Bank.all().order('name')
+    banks = banks_query.fetch(10)
+
+
+    template_values = {
+      'accounts': accounts,
+      'banks': banks,
+      'currentUser': users.get_current_user(),
+      'loginUrl': users.create_login_url("/"),
+      'logoutUrl': users.create_logout_url("/"),
+      }
+    path = os.path.join(os.path.dirname(__file__), 'account.html')
+    self.response.out.write(template.render(path, template_values))
+
+  def post(self):
+    self.handleActions()
+
+class BankPage(webapp.RequestHandler):
+  def add(self):
+    if not users.is_current_user_admin():
+      return
+    bank = Bank(
+      name = self.request.get('name'),
+      )
+    bank.put()
+
+
+  def delete(self):
+#TODO: delete bank's operation.
+    if not users.is_current_user_admin():
+      return
+    if self.request.get('key') == None:
+      return
+    bank = db.get(self.request.get('key'))
+    bank.delete()
+
+  def handleActions(self):
+    if self.request.get('action') == 'delete':
+      self.delete()
+      self.redirect('/bank')
+    if self.request.get('action') == 'add':
+      self.add()
+      self.redirect('/bank')
+
+  def get(self):
+    self.handleActions()
+    banks_query = Bank.all().order('name')
+    banks = banks_query.fetch(10)
+
+    template_values = {
+      'banks': banks,
+      'currentUser': users.get_current_user(),
+      'loginUrl': users.create_login_url("/"),
+      'logoutUrl': users.create_logout_url("/"),
+      }
+    path = os.path.join(os.path.dirname(__file__), 'bank.html')
+    self.response.out.write(template.render(path, template_values))
+
+
+  def post(self):
+    self.handleActions()
+
+class OperationPage(webapp.RequestHandler):
+  def add(self):
+    if users.get_current_user() == None:
+      return
+    operation = Operation(
+      description = self.request.get('description'),
+      account = db.get(self.request.get('account')),
+      amount = self.request.get('amount')
+      )
+    operation.put()
+
+  def delete(self):
+    if users.get_current_user() == None:
+      return
+    if self.request.get('key') == None:
+      return
+    operation = db.get(self.request.get('key'))
+    if operation.account.owner == users.get_current_user():
+      operation.delete()
+
+  def handleActions(self):
+    if self.request.get('action') == 'delete':
+      self.delete()
+      self.redirect('/operation?account=' + self.request.get('account'))
+    if self.request.get('action') == 'add':
+      self.add()
+      self.redirect('/operation?account=' + self.request.get('account'))
+
+  def get(self):
+    self.handleActions()
+
+    if not self.request.get('account'):
+      self.redirect('/')
+      return
+    if users.get_current_user() == None:
+      self.redirect('/')
+      return
+
+    account = db.get (self.request.get('account'))
+    if not account or account.owner != users.get_current_user():
+      self.redirect('/')
+      return
+
+    operations_query = Operation.all()
+    operations_query.filter('account =', db.get(self.request.get('account')))
+    operations = operations_query.fetch(10)
+
+    currentAmount=Decimal()
+    for op in operations:
+      currentAmount += Decimal(op.amount)
+
+    template_values = {
+      'account': account,
+      'operations': operations,
+      'currentAmount': currentAmount,
+      'currentUser': users.get_current_user(),
+      'loginUrl': users.create_login_url("/"),
+      'logoutUrl': users.create_logout_url("/"),
+      }
+    path = os.path.join(os.path.dirname(__file__), 'operation.html')
+    self.response.out.write(template.render(path, template_values))
+
+  def post(self):
+    self.handleActions()
 
 
 def main():
-  application = webapp.WSGIApplication([('/', MainHandler)],
+  application = webapp.WSGIApplication([('/', AccountPage),
+                                        ('/bank', BankPage),
+                                        ('/operation', OperationPage)],
                                        debug=True)
-  wsgiref.handlers.CGIHandler().run(application)
+  run_wsgi_app(application)
 
 
 if __name__ == '__main__':
